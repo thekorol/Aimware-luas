b1477ce5618b9f7fee5756de0ad84a0a9a2ad8be
thekorol <66296026+thekorol@users.noreply.github.com>
2021-06-26 12:39:31 +0300

    Update OOV.lua

--- OOV.lua
+++ OOV.lua
@@ -1,15 +1,38 @@
+local OOV_always = gui.Checkbox(gui.Reference("Visuals", "Local", "Helper"),"OOV.always", "Consider FOV", false);
 local OOV_color = gui.ColorPicker(gui.Reference("Visuals", "Local", "Helper"), "OOV.clr", "Out Of Viev Color", 255, 255, 255, 255)
 
-local function require(filename, url)
-    local filename = filename .. ".lua"
-    local function http_write(body)
-        file.Write(filename, body)
-    end
-    local module = RunScript(filename) or http.Get(url, http_write)
-    return module or error("unable to load module " .. filename, 2)
+local function bad_argument(expression, name, expected)
+    assert(type(expression) == expected, " bad argument #1 to '%s' (%s expected, got %s)", 4, name, expected, tostring(type(expression)))
 end
-local renderer = require("libraries/renderer", "https://raw.githubusercontent.com/287871/aimware/renderer/renderer.lua")
 
+function renderer.circle_outline(x, y, r, g, b, a, radius, start_degrees, percentage, thickness, radian)
+    bad_argument(x and y and radius and start_degrees and percentage and thickness, "circle_outline", "number")
+
+    local thickness = radius - thickness
+    local percentage = math.abs(percentage * 360)
+    local radian = radian or 1
+
+    draw.Color(r, g, b, a)
+
+    for i = start_degrees + radian, start_degrees + percentage, radian do
+        local cos_1 = math.cos(i * math.pi / 180)
+        local sin_1 = math.sin(i * math.pi / 180)
+        local cos_2 = math.cos((i + radian) * math.pi / 180)
+        local sin_2 = math.sin((i + radian) * math.pi / 180)
+
+        local x0 = x + cos_2 * thickness
+        local y0 = y + sin_2 * thickness
+        local x1 = x + cos_1 * radius
+        local y1 = y + sin_1 * radius
+        local x2 = x + cos_2 * radius
+        local y2 = y + sin_2 * radius
+        local x3 = x + cos_1 * thickness
+        local y3 = y + sin_1 * thickness
+
+        draw.Triangle(x1, y1, x2, y2, x3, y3)
+        draw.Triangle(x3, y3, x2, y2, x0, y + sin_2 * thickness)
+    end
+end
 local function clamp(val, min, max)
     if val > max then
         return max
@@ -67,11 +90,13 @@ local function Draw()
     for k, v in pairs(temp) do
         local index = v:GetIndex()
         local v_abs = v:GetAbsOrigin()
+		local psx,psy = client.WorldToScreen(v_abs);
         angle = (v_abs - lp_abs):Angles()
         angle.y = angle.y - view_angles.y
         for i = 1, 2, 0.2 do
             local alpha = i / 5 * alpha[index]
-
+			
+			if psx == nil and psy == nil or psx < 100 or psy < 100 or psx > screen_size_x+500 or psy > screen_size_y+100 or not OOV_always:GetValue() then					
             renderer.circle_outline(
                 screen_size_x,
                 screen_size_y,
@@ -79,9 +104,9 @@ local function Draw()
                 g,
                 b,
                 alpha,
-                (65 + i),
+                (125 + i),
                 (270 - 0.13 * 170) - angle.y + (i * 0.2),
-                0.13 + (i * 0.00005),
+                0.075 + (i * 0.00005),
                 (i * 2)
             )
 			renderer.circle_outline(
@@ -91,11 +116,12 @@ local function Draw()
                 g,
                 b,
                 alpha,
-                (70 + i),
+                (130 + i),
                 (270 - 0.13 * 170) - angle.y + (i * 0.2),
-                0.13 + (i * 0.00005),
+                0.075 + (i * 0.00005),
                 (i * 0.5)
-            )
+            )		
+			end
         end
     end
 end
